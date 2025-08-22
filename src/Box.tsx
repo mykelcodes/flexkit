baef3dfd641d80a9d5e6e25d729b33780ab82ccd
Michael Olawale <tech@mykelcodes.com>
2025-08-22 10:22:21 +0100

    feat: added unistyles to example app

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,14 +1,19 @@
 import { View, type ViewProps } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
 
 type Props = ViewProps & {
     bg?: string;
 };
 
 export function Box({ bg, style, ...props }: Props) {
-    return (
-        <View
-            {...props}
-            style={[{ backgroundColor: bg, flex: 1, alignItems: 'center', paddingTop: 24 }, style]}
-        />
-    );
+    return <View {...props} style={[styles.box, style]} />;
 }
+
+const styles = StyleSheet.create({
+    box: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 24,
+        paddingHorizontal: 24,
+    },
+});
